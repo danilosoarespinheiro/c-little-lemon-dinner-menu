33da470d9e525b275eb3cbd37b296d8aaf8cbbaf
danilosoarespinheiro <danilosoarespinheiro@gmail.com>
2024-02-02 11:23:34 -0400

    Tasks 6 to 8

--- LittleLemonMenu/app/src/main/java/com/littlelemon/menu/FilterHelper.kt
+++ LittleLemonMenu/app/src/main/java/com/littlelemon/menu/FilterHelper.kt
@@ -1,14 +1,21 @@
 package com.littlelemon.menu
 
+import com.littlelemon.menu.FilterType.All
+import com.littlelemon.menu.FilterType.Dessert
+import com.littlelemon.menu.FilterType.Drinks
+import com.littlelemon.menu.FilterType.Food
+
 class FilterHelper {//TODO create a FilterHelperTest and write a unit test for filterProducts
 
     fun filterProducts(type: FilterType, productsList: List<ProductItem>): List<ProductItem> {
         return when (type) {
-            FilterType.All -> productsList
-            FilterType.Dessert -> TODO("only products with category equal to Dessert")
-            FilterType.Drinks -> TODO("only products with category equal to Drinks")
-            FilterType.Food -> TODO("only products with category equal to Food")
+            All -> productsList
+            Dessert -> productsList.productItemFilter("Dessert")
+            Drinks -> productsList.productItemFilter("Drinks")
+            Food -> productsList.productItemFilter("Food")
         }
     }
 
+    private fun List<ProductItem>.productItemFilter(filter: String) =
+        filter { productItem -> productItem.category == filter }
 }
--- LittleLemonMenu/app/src/main/java/com/littlelemon/menu/SortHelper.kt
+++ LittleLemonMenu/app/src/main/java/com/littlelemon/menu/SortHelper.kt
@@ -1,17 +1,22 @@
 package com.littlelemon.menu
 
+import com.littlelemon.menu.SortType.Alphabetically
+import com.littlelemon.menu.SortType.PriceAsc
+import com.littlelemon.menu.SortType.PriceDesc
+
 class SortHelper {
 
     fun sortProducts(type: SortType, productsList: List<ProductItem>): List<ProductItem> {
         return when (type) {
-            SortType.Alphabetically -> productsList.sortAlphabetically()
-            SortType.PriceAsc -> TODO("create extension function for List<Product> with sorting based on price ascending")
-            SortType.PriceDesc -> TODO("create extension function for List<Product> with sorting based on price descending")
+            Alphabetically -> productsList.sortAlphabetically()
+            PriceAsc -> productsList.sortPriceAsc()
+            PriceDesc -> productsList.sortPriceDesc()
         }
     }
 
-    private fun List<ProductItem>.sortAlphabetically(): List<ProductItem> {
-        return sortedBy { it.title }
-    }
+    private fun List<ProductItem>.sortAlphabetically() = sortedBy { it.title }
 
+    private fun List<ProductItem>.sortPriceAsc() = sortedBy { it.price }
+
+    private fun List<ProductItem>.sortPriceDesc() = sortedByDescending { it.price }
 }
